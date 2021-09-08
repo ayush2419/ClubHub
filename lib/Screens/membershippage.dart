5e2d17b68fcb570569d76d9e8de5b73b3127b275
Ayush <ayush.singh1@somaiya.edu>
2021-09-08 21:11:39 +0530

    a2

--- lib/Screens/membershippage.dart
+++ lib/Screens/membershippage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:club_hub/utilites/bottombar.dart';
 
 class MembershipPage extends StatefulWidget {
   const MembershipPage({Key? key}) : super(key: key);
@@ -10,6 +12,32 @@ class MembershipPage extends StatefulWidget {
 class _MembershipPageState extends State<MembershipPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Membership',
+          style: TextStyle(
+              fontSize: 40.00,
+              fontWeight: FontWeight.bold,
+              color: Colors.black),
+        ),
+        backgroundColor: Color(0xFFb3c8ff),
+        leading: Icon(
+          FontAwesomeIcons.bars,
+          color: Colors.black,
+        ),
+        actions: [
+          Icon(
+            FontAwesomeIcons.ellipsisV,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 15.00,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomBar(),
+    );
   }
 }
