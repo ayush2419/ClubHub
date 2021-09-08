d3519ec95c0059f13c20d56c93f4dc53c04b5904
Ayush <ayush.singh1@somaiya.edu>
2021-09-08 13:02:50 +0530

    first commit

---
+++ lib/Screens/membershippage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MembershipPage extends StatefulWidget {
+  const MembershipPage({Key? key}) : super(key: key);
+
+  @override
+  _MembershipPageState createState() => _MembershipPageState();
+}
+
+class _MembershipPageState extends State<MembershipPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'Screens/homepage.dart';
+import 'Screens/profilepage.dart';
+import 'Screens/bookingpage.dart';
+import 'Screens/membershippage.dart';
+import 'Screens/loginpage.dart';
+import 'Screens/signuppage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      //First Screen to be displayed when the App starts
+      initialRoute: '/login',
+
+      //Routes of All pages(To be Modified)
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+        '/booking': (context) => BookingPage(),
+        '/profile': (context) => ProfilePage(),
+        'membership': (context) => MembershipPage(),
+      },
+    );
+  }
+}
