e6ed302e00c105ffbc639c4f85ff89ee4f734f2a
Ayush <ayush.singh1@somaiya.edu>
2021-09-08 15:26:36 +0530

    drawer

--- lib/utilites/drawer.dart
+++ lib/utilites/drawer.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
 //left app drawer
+
+class LeftAppDraw extends StatefulWidget {
+  const LeftAppDraw({ Key? key }) : super(key: key);
+
+  @override
+  _LeftAppDrawState createState() => _LeftAppDrawState();
+}
+
+class _LeftAppDrawState extends State<LeftAppDraw> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
